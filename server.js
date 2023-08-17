8164daa4f3f375e725de1c28e0aa255231fc6dae
Brady Bott <86979515+DaTrueBrad@users.noreply.github.com>
2023-08-17 08:21:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '4aaafae94fdc44f39283fc2ee1b5f889',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/api/cat', (req, res) => {
+    rollbar.log('Got a cat!')
     res.status(200).send('Best regards from Flavio the cat.')
 })
 
